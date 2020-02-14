9baa3c0444a4c1f9c54d0e0b65643716e658d6f3
Jayant Dhingra <jayantdhingra1441@gmail.com>
2020-02-15 02:29:14 +0530

    5th commit

---
+++ app/src/main/java/com/example/enactus/PrivacyPolicy.kt
@@ -0,0 +1,16 @@
+package com.example.enactus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_privacy_policy.*
+
+class PrivacyPolicy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy_policy)
+
+        tv_privacy_policy_title.text = intent.extras!!.getString("privacy_policy_title")
+        tv_privacy_policy_des.text = intent.extras!!.getString("privacy_policy_des")
+    }
+}
