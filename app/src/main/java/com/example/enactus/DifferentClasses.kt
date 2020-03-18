d322a7809d84faee87f7159c194c4c7e20a2ad28
Jayant Dhingra <jayantdhingra1441@gmail.com>
2020-03-18 21:17:47 +0530

    Final commit 2

---
+++ app/src/main/java/com/example/enactus/DifferentClasses.kt
@@ -0,0 +1,20 @@
+package com.example.enactus
+
+class HeightWeightDataClass(var weight: String, var height: String){
+    constructor():this("","")
+}
+
+class wakeUpTimeDataClass(var wakeUpTime: String){
+    constructor():this("")
+}
+
+class SleepTimeDataClass(var sleepTime: String){
+    constructor():this("")
+}
+
+class update_lastDataClass(var firstDayOFPeriod: String?, var duration_of_period: String?, var recurrence : String?){
+    constructor():this("","","")
+}
+
+
+
